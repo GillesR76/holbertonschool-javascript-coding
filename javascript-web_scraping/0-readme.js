ab7fed340c7ddc25a9ece044ec4e14403776f3ed
GillesR76 <gilles-richard76@hotmail.com>
2024-05-13 11:03:48 +0200

    feat: Update error handling in JavaScript web scraping code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, data) => {
+
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }
