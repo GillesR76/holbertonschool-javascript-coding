5a0e85f71143c8439cead0e8ac914910b9b59011
GillesR76 <gilles-richard76@hotmail.com>
2024-05-13 12:14:20 +0200

    Update error handling in JavaScript web scraping code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,7 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
   if (error) {
-    console.error('error:', error);
+    console.error(error);
   } else {
     console.log(`code: ${response.statusCode}`);
   }
