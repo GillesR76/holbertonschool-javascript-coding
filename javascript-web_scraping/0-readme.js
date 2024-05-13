da3d7b7801daa7b9d9cade65132e51c1f3f23d6c
GillesR76 <gilles-richard76@hotmail.com>
2024-05-13 11:29:38 +0200

    feat: Refactor JavaScript web scraping code for improved readability and maintainability

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+// imports the built-in Node.js 'fs' (file system) module.
 
 const filePath = process.argv[2];
 
+// calling the readFile functionfrom the fs module
+// It reads a file with the provided file path
+// It then calls the provided callback function
+// with two arguments: an error object and the
+// data from the file
 fs.readFile(filePath, 'utf8', (err, data) => {
+  // body of the callback funtion:
   if (err) {
     console.log(err);
   } else {
