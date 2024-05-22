27dc7313352df9d52c6a790ccdd3f234cd779ccb
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 19:38:09 +0200

    refactor readline interface variable name in NodeJS basic console module

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
+const talk = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+talk.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}!`);
-  rl.close();
+  talk.close();
+});
+
+talk.on('close', () => {
   console.log('This important software is now closing');
 });
