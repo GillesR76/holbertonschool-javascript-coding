798e30b9f5f7b5f0e53b5932ade4872803253afb
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 19:24:54 +0200

    feat: Add NodeJS basic console module

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}!`);
+  rl.close();
+  console.log('This important software is now closing');
+});
