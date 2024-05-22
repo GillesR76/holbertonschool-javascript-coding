98cab1ac9251d41ac22c23195b2ffcbb333f705d
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 22:31:14 +0200

    Refactor NodeJS basic console module to use readline interface for input and output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,10 +7,9 @@ const talk = readline.createInterface({
 
 talk.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}!`);
-
-  process.on('exit', () => {
-    console.log('This important software is now closing\n');
-  });
-
   talk.close();
+});
+
+talk.on('close', () => {
+  console.log('This important software is now closing');
 });
