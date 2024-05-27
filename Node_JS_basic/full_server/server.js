3c6a67f86289f3f805ff948b5d84e8ba2531bb7c
GillesR76 <gilles-richard76@hotmail.com>
2024-05-27 21:54:46 +0200

    refactor server.js to use express framework for routing

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const indexRoutes = require("./routes/index");
+const express = require('express');
+const indexRoutes = require('./routes/index');
 
 const app = express();
 
-app.use("/", indexRoutes);
+app.use('/', indexRoutes);
 
 app.listen(1245, () => {
-  console.log("Application est lancée sur le port 1245");
+  console.log('Application est lancée sur le port 1245');
 });
 
 module.exports = app;
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function readDatabase(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(err);
         return;
       }
-      const lines = data.split("\n");
+      const lines = data.split('\n');
       const students = lines.slice(1).filter((line) => line.length > 0);
       const fields = {};
       for (const student of students) {
-        const field = student.split(",")[3];
-        const firstName = student.split(",")[0];
+        const field = student.split(',')[3];
+        const firstName = student.split(',')[0];
         if (!fields[field]) {
           fields[field] = [];
         }
