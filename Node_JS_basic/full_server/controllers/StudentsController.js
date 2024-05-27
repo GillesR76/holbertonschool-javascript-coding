29b9c0c1faa0a8947802bdd5f4b878dbf2ed08b4
GillesR76 <gilles-richard76@hotmail.com>
2024-05-27 21:56:21 +0200

    Refactor StudentsController to use consistent quotes and improve error handling

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,42 +1,40 @@
-const readDatabase = require("../utils");
+const readDatabase = require('../utils');
 
 class StudentsController {
   static getAllStudents(req, res) {
     readDatabase(process.argv[2])
       .then((data) => {
-        let output = "This is the list of our students\n";
-        const fields = Object.keys(data).sort((a, b) =>
-          a.localeCompare(b, "en", { sensitivity: "base" })
-        );
+        let output = 'This is the list of our students\n';
+        const fields = Object.keys(data).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
         for (const field of fields) {
           output += `Number of students in ${field}: ${
             data[field].length
-          }. List: ${data[field].join(", ")}\n`;
+          }. List: ${data[field].join(', ')}\n`;
         }
         res.status(200).send(output);
       })
       .catch(() => {
-        res.status(500).send("Cannot load the database");
+        res.status(500).send('Cannot load the database');
       });
   }
 
   static getAllStudentsByMajor(req, res) {
     const { major } = req.params;
-    if (!["CS", "SWE"].includes(major)) {
-      res.status(500).send("Major parameter must be CS or SWE");
+    if (!['CS', 'SWE'].includes(major)) {
+      res.status(500).send('Major parameter must be CS or SWE');
       return;
     }
     readDatabase(process.argv[2])
       .then((data) => {
         if (!data[major]) {
-          res.status(500).send("Major not found");
+          res.status(500).send('Major not found');
           return;
         }
-        const output = `List: ${data[major].join(", ")}`;
+        const output = `List: ${data[major].join(', ')}`;
         res.status(200).send(output);
       })
       .catch(() => {
-        res.status(500).send("Cannot load the database");
+        res.status(500).send('Cannot load the database');
       });
   }
 }
