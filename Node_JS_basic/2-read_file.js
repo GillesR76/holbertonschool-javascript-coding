65982553eb57c840d0da56bbecca3ef99efac098
GillesR76 <gilles-richard76@hotmail.com>
2024-05-27 17:14:18 +0200

    Refactor NodeJS basic console module to use readline interface for input and output

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+function countStudents(path) {
+  let data;
+
+  try {
+    const data = fs.readFileSync(path, "utf8");
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+
+  const lines = data.split("\n");
+  const students = lines
+    .slice(1)
+    .filter((line) => line)
+    .map((line) => line.split(","));
+
+  console.log(`Number of students: ${students.length}`);
+
+  const fields = {};
+  for (const student of students) {
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(student[0]);
+  }
+
+  for (const field in fields) {
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[
+        field
+      ].join(", ")}`
+    );
+  }
+}
+
+module.exports = countStudents;
