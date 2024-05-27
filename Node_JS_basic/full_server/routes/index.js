a804a1bd8bc1957cb44c60c691934647b65b8e7d
GillesR76 <gilles-richard76@hotmail.com>
2024-05-27 22:08:57 +0200

    Refactor routes to use consistent URL paths and improve readability

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -5,7 +5,7 @@ const StudentsController = require('../controllers/StudentsController');
 const AppController = require('../controllers/AppController');
 
 router.get('/', AppController.getHomepage);
-router.get('students', StudentsController.getAllStudents);
-router.get('students/:major', StudentsController.getAllStudentsByMajor);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 module.exports = router;
